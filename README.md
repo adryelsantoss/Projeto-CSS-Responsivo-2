4e620cf5e48791cf01a62a82260da895460f200d
adryelsantoss <adryel_cps@hotmail.com>
2025-10-30 19:25:27 -0300

    feat: funcionalidades em JavaScript + README

---
+++ README.md
@@ -0,0 +1,47 @@
+# 🌐 Landing Page: Wide Coverage Location
+
+![Mockup da Landing Page - En](https://github.com/adryelsantoss/Projeto-CSS-Responsivo-2/blob/master/docs/mockup%20en-usa.png) 
+
+![Mockup da Landing Page - Pt](https://github.com/adryelsantoss/Projeto-CSS-Responsivo-2/blob/master/docs/mockup%20pt-br.png) 
+
+<br> 
+
+Este projeto foi desenvolvido como parte de um desafio de aprendizado em desenvolvimento web, com o objetivo de praticar HTML, CSS, tendo JavaScript adicionado posteriormente para aprimorar a interatividade.
+
+<br>
+
+## 📌 Sobre o Projeto
+
+A proposta foi criar uma landing page moderna e responsiva, que apresente um produto ou serviço de forma simples e visualmente agradável.
+
+Além disso, a página possui a funcionalidade de troca de idioma entre Português e Inglês, demonstrando o uso de manipulação de DOM e interatividade com JavaScript.
+
+- O foco principal foi aprimorar:
+
+- A estrutura semântica com HTML.
+
+- A estilização e responsividade com CSS.
+
+- E a lógica e dinamicidade com JavaScript.
+
+<br>
+
+## ⚙️ Funcionalidades
+
+🔁 Troca dinâmica de idioma (Português 🇧🇷 / Inglês 🇺🇸)
+
+📱 Layout responsivo, adaptável para telas desktop e mobile
+
+✨ Transição suave ao alternar o idioma
+
+🧭 Navegação no cabeçalho com links simulados
+
+🎨 Design limpo e moderno
+
+<br>
+
+## 💻 Tecnologias usadas
+
+- HTML5
+- CSS3
+- JavaScript"
---
+++ script.js
@@ -0,0 +1,70 @@
+const changedBr = document.querySelector(".id-br")
+const changedUsa = document.querySelector(".id-usa")
+const title = document.querySelector(".title")
+const text = document.querySelector(".text")
+const products = document.querySelector("#products")
+const company = document.querySelector("#company")
+const learnMore = document.querySelector(".link-section")
+const signUp = document.querySelector(".menu-button")
+const login = document.querySelector("#login")
+let currentLang = "";
+
+function changedIdBr() {
+    currentLang = "br";
+    title.textContent = `Local de Ampla Cobertura`;
+    text.textContent =
+        `Corridas compartilhadas: Há muito o que fazer na sua cidade, 
+         e nós temos as opções de transporte para você aproveitar tudo. 
+         E tudo isso está disponível diretamente no seu aplicativo.`;
+    products.textContent = `Produtos`;
+    company.textContent = `Empresa`;
+    signUp.textContent = `Inscrever-se`;
+    learnMore.textContent = `Saiba Mais`;
+    login.textContent = `Entrar`;
+
+    styleSettings()
+}
+
+function changedIdUsa() {
+    currentLang = "usa";
+    title.textContent = `Wide Coverage Location`;
+    text.textContent =
+        `Shared rides: There’s lots to do in
+         your city, and we’ve got the rides
+         to help you enjoy it all. And they’re
+         all available right from your app.`;
+    products.textContent = `Products`;
+    company.textContent = `Company`;
+    signUp.textContent = `Sign Up`;
+    learnMore.textContent = `Learn More`;
+    login.textContent = `Login`
+
+    styleSettings()
+}
+
+function styleSettings() {
+
+    if (window.matchMedia("(max-width:900px)").matches) {
+        if (currentLang === "br") {
+            text.style.width = "340px";
+        } else if (currentLang === "usa") {
+            text.style.width = "304px";
+        }
+    } else {
+        text.style.width = "493px";
+    }
+}
+function fadeContent(callback) {
+  document.body.style.transition = "opacity 0.7s";
+  document.body.style.opacity = 0;
+
+  setTimeout(() => {
+    callback(); // muda o idioma
+    document.body.style.opacity = 1;
+  }, 700);
+}
+
+
+window.addEventListener('resize', styleSettings)
+changedBr.addEventListener('click', () => fadeContent(changedIdBr));
+changedUsa.addEventListener('click', () => fadeContent(changedIdUsa));
